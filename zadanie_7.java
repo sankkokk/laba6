3d3f3f6840062e51113ba5e7c3d3f19cec6d84c0
sankkokk <118100738+sankkokk@users.noreply.github.com>
2023-01-05 21:44:05 +0500

    Create zadanie_7.java

---
+++ zadanie_7.java
@@ -0,0 +1,24 @@
+public class ufff {
+    public static void main(String[] args){
+        String[][] table = tableArray(9, 9);
+        StringBuilder tableF = new StringBuilder();
+
+        for (int line = 0; line < table.length; line++){
+            for (int column = 0; column < table[line].length; column++){
+                tableF.append(line + 1).append(" x ").append(column + 1).append(" = ").append(table[line][column]).append("\t");
+            }
+            System.out.println(tableF);
+            tableF = new StringBuilder();
+        }
+    }
+
+    public static String[][] tableArray(int lines, int columns){
+        String[][] tableCount = new String[lines][columns];
+        for (int line = 0; line < tableCount.length; line++){
+            for (int column = 0; column < tableCount[line].length; column++){
+                tableCount[line][column] = String.valueOf((line + 1)*(column + 1));
+            }
+        }
+        return tableCount;
+    }
+}
